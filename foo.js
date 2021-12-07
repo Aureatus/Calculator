c85e68b212d8d61521d13142c19ef81d0acb815a
Zesty Alex <alextknott@gmail.com>
2021-12-07 23:28:44 +0000

    Rebuilt some of the initial functions

--- foo.js
+++ foo.js
@@ -1,4 +1,91 @@
-/* Operation function declaration */
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1-num2
+
+}
+
+function multiply(num1, num2) {
+    return num1*num2
+}
+
+function divide(num1, num2) {
+    return num1/num2
+}
+
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        return add(num1,num2)
+    } 
+    else if (operator === "-") {
+        return subtract(num1,num2)
+    }
+    else if (operator === "*") {
+        return multiply(num1,num2)
+    }
+    else if (operator === "/") {
+        return divide(num1,num2)
+    }
+}
+
+function buttonEventListenerAdd(target, listener, functionToAdd) {
+    for(let i=0; i < target.length; i++) {
+        target[i].addEventListener(listener, functionToAdd);
+    }
+}
+
+const display = document.querySelector("#display")
+const numberButton = document.querySelectorAll(".number")
+const operatorButton = document.querySelectorAll(".operator")
+let displayValue = display.textContent
+
+buttonEventListenerAdd(numberButton, "click", numberButtonPress)
+buttonEventListenerAdd(operatorButton, "click", operatorButtonPress)
+function numberButtonPress() {
+    textWrite(this)
+}
+
+function textWrite(input) {
+    let textToWrite = input.textContent;
+    display.textContent += textToWrite;
+} 
+
+function operatorButtonPress () {
+    textWrite(this)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function add(num1, num2) {
     return num1 + num2
 }
@@ -95,17 +182,13 @@ function operatorButtonPress() {
 }
 
 function calculation() {
-    answer = operate(chosenOperator,Number(number1),Number(number2))
+    answer = operate(Number(chosenOperator,number1,number2))
     return
 }
 
 function equals() {
     calculation()
-    display.textContent = answer
-    hasOperatorBeenChosen = false
-    chosenOperator = null
-    number1 = answer
-    number2 = null
+    console.log(answer)
 }
 
 function keyChecker(input) {
@@ -119,5 +202,4 @@ function keyChecker(input) {
 
 buttonEventListenerAdd(numberButton, "click", numberButtonPress)
 buttonEventListenerAdd(operatorButton, "click", operatorButtonPress)
-document.addEventListener("keydown", keyChecker)
-/* Restart tomorrow and attempt again */
+document.addEventListener("keydown", keyChecker)*/
